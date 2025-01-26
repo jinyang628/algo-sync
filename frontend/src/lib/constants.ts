075ebe1bb5cf23c6927877979ae86d5e5259cf23
jinyang628 <chenjinyang4192@gmail.com>
2025-01-26 13:24:18 -0500

    Update

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,1 +1,1 @@
-export const SERVER_BASE_URL = 'http://localhost:8080';
+export const SERVER_BASE_URL = 'https://algo-sync.onrender.com';
--- server/app/services/users.py
+++ server/app/services/users.py
@@ -1,8 +1,16 @@
+import os
+
+from dotenv import find_dotenv, load_dotenv
+
 from app.models.users import AuthenticateResponse
+
+load_dotenv()
+
+SERVER_BASE_URL = os.getenv("SERVER_BASE_URL")
 
 
 class UsersService:
-    async def authenticate(
-        self, client_id: str, client_secret: str
-    ) -> AuthenticateResponse:
-        pass
+    async def authenticate(self, client_id: str, client_secret: str) -> AuthenticateResponse:
+        print(client_id)
+        print(client_secret)
+        print(SERVER_BASE_URL)
